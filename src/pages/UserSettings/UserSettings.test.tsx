6a5b2539c775715a432a63cb729df204ac3efe2e
guiazca <guiazca@gmail.com>
2024-10-26 20:49:41 +0100

    fixing prepush

--- src/pages/UserSettings/UserSettings.test.tsx
+++ src/pages/UserSettings/UserSettings.test.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { describe, it, beforeEach, vi } from 'vitest';
+import {
+  act,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+} from '@testing-library/react';
+import { describe, it, beforeEach, MockedFunction } from 'vitest';
+import { vi } from 'vitest';
 import { UserSettings } from './UserSettings';
 import { useAuthStore } from '@store/api/userApi/useAuthStore';
 import { useUserSettingsStore } from '@store/userSettings/store';
 import '@tests/i18nTest';
+import userEvent from '@testing-library/user-event';
 
 // Mock das stores e hooks
 vi.mock('@store/api/userApi/useAuthStore');
@@ -12,13 +20,17 @@ vi.mock('@store/userSettings/store');
 
 describe('UserSettings Component', () => {
   beforeEach(() => {
-    (useAuthStore as jest.MockedFunction<typeof useAuthStore>).mockReturnValue({
+    (
+      useAuthStore as unknown as MockedFunction<typeof useAuthStore>
+    ).mockReturnValue({
       updateUser: vi.fn(),
       isLoading: false,
     });
 
     (
-      useUserSettingsStore as jest.MockedFunction<typeof useUserSettingsStore>
+      useUserSettingsStore as unknown as MockedFunction<
+        typeof useUserSettingsStore
+      >
     ).mockReturnValue({
       darkModeEnabled: false,
       notificationsEnabled: true,
@@ -43,39 +55,39 @@ describe('UserSettings Component', () => {
     expect(screen.getByText('Language')).toBeInTheDocument();
   });
 
-  //   it('should call updateUser function on saving settings', async () => {
-  //     const mockUpdateUser = vi.fn();
-  //     (useAuthStore as jest.MockedFunction<typeof useAuthStore>).mockReturnValue({
-  //       updateUser: mockUpdateUser,
-  //       isLoading: false,
-  //     });
-
-  //     render(<UserSettings />);
-
-  //     const nameInput = screen.getByText('Name');
-  //     const addressInput = screen.getByText('Address');
-  //     const phoneInput = screen.getByText('Phone');
-  //     //fireEvent.input(nameInput, { target: { value: 'John Doe' } });
-  //     await act(async () => {
-  //         fireEvent.change(nameInput, { target: { value: 'John Doe' } });
-  //         await userEvent.type(screen.getByText('Name'), 'John Doe' );
-  //         await userEvent.type(screen.getByText('Address'), '123 Main St');
-  //         await userEvent.type(screen.getByText('Phone'), '123-456-7890' );
-  //     });
-  //     // fireEvent.input(screen.getByLabelText('Endereço'), {
-  //     //   target: { value: '123 Main St' },
-  //     // });
-  //     // fireEvent.input(screen.getByLabelText('Telefone'), {
-  //     //   target: { value: '123-456-7890' },
-  //     // });
-
-  //     //fireEvent.click(screen.getByRole('button', { name: /Salvar Configurações/i }));
-
-  //     await userEvent.click(screen.getByText('Salvar Configurações'));
-  //     await waitFor(() => {
-  //       expect(mockUpdateUser).toHaveBeenCalledWith('John Doe', '123 Main St', '123-456-7890');
-  //     });
+  // it('should call updateUser function on saving settings', async () => {
+  //   const mockUpdateUser = vi.fn();
+  //   (useAuthStore as jest.MockedFunction<typeof useAuthStore>).mockReturnValue({
+  //     updateUser: mockUpdateUser,
+  //     isLoading: false,
   //   });
+
+  //   render(<UserSettings />);
+
+  //   const nameInput = screen.getByText('Name');
+  //   const addressInput = screen.getByText('Address');
+  //   const phoneInput = screen.getByText('Phone');
+  //   //fireEvent.input(nameInput, { target: { value: 'John Doe' } });
+  //   await act(async () => {
+  //       //fireEvent.change(nameInput, { target: { value: 'John Doe' } });
+  //       await userEvent.type(screen.getByText('Name'), 'John Doe' );
+  //       await userEvent.type(screen.getByText('Address'), '123 Main St');
+  //       await userEvent.type(screen.getByText('Phone'), '123-456-7890' );
+  //   });
+  //   // fireEvent.input(screen.getByLabelText('Endereço'), {
+  //   //   target: { value: '123 Main St' },
+  //   // });
+  //   // fireEvent.input(screen.getByLabelText('Telefone'), {
+  //   //   target: { value: '123-456-7890' },
+  //   // });
+
+  //   //fireEvent.click(screen.getByRole('button', { name: /Salvar Configurações/i }));
+
+  //   await userEvent.click(screen.getByText('Salvar Configurações'));
+  //   await waitFor(() => {
+  //     expect(mockUpdateUser).toHaveBeenCalledWith('John Doe', '123 Main St', '123-456-7890');
+  //   });
+  // });
 
   //   it('should toggle dark mode when Dark Mode toggle is changed', () => {
   //     const mockSetDarkMode = vi.fn();
