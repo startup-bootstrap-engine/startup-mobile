fed686b448273929689eb4c452c84e4dd3ed88a1
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-11-12 14:26:02 -0800

    feat: enhance FormField component to support multiple input types and improve type handling

--- src/components/forms/FormField.tsx
+++ src/components/forms/FormField.tsx
@@ -1,12 +1,21 @@
-import { IonInput, IonItem, IonText } from '@ionic/react';
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import type { TextFieldTypes } from '@ionic/core';
+import {
+  IonCheckbox,
+  IonDatetime,
+  IonInput,
+  IonItem,
+  IonRadio,
+  IonText,
+} from '@ionic/react';
 import React from 'react';
 
 interface IProps {
   label: string;
-  value: string;
-
-  onChange: (value: string) => void;
-  type?: 'text' | 'email' | 'password';
+  value: string | number | boolean;
+  onChange: (value: string | number | boolean) => void;
+  type?: TextFieldTypes | 'date' | 'checkbox' | 'radio';
   error?: string | null;
   placeholder?: string;
   clearInput?: boolean;
@@ -16,7 +25,7 @@ interface IProps {
   className?: string;
 }
 
-export const FormField: React.FC<IProps> = ({
+const FormField: React.FC<IProps> = ({
   label,
   value,
   onChange,
@@ -29,23 +38,82 @@ export const FormField: React.FC<IProps> = ({
   readonly = false,
   className,
 }) => {
+  const renderInput = () => {
+    switch (type) {
+      case 'text':
+      case 'email':
+      case 'password':
+      case 'number':
+        return (
+          <IonInput
+            label={label}
+            labelPlacement="floating"
+            type={type}
+            value={value as string}
+            onIonChange={(e) => onChange(e.detail.value || '')}
+            placeholder={placeholder}
+            clearInput={clearInput}
+            required={required}
+            disabled={disabled}
+            readonly={readonly}
+            className={error ? 'ion-invalid' : ''}
+          />
+        );
+      case 'date':
+        return (
+          <IonDatetime
+            value={value as string}
+            onIonChange={(e) => onChange(e.detail.value as any)}
+            disabled={disabled}
+            readonly={readonly}
+            className={error ? 'ion-invalid' : ''}
+          />
+        );
+      case 'checkbox':
+        return (
+          <IonCheckbox
+            labelPlacement="end"
+            slot="start"
+            checked={value as boolean}
+            onIonChange={(e) => onChange(e.detail.checked)}
+          >
+            {label}
+          </IonCheckbox>
+        );
+      case 'radio':
+        return (
+          <IonRadio
+            labelPlacement="end"
+            slot="start"
+            value={value as string}
+            // eslint-disable-next-line no-undef
+            onChange={(e) => onChange((e.target as HTMLIonRadioElement).value!)}
+          >
+            {label}
+          </IonRadio>
+        );
+      default:
+        return (
+          <IonInput
+            label={label}
+            labelPlacement="floating"
+            type="text"
+            value={value as string}
+            onIonChange={(e) => onChange(e.detail.value || '')}
+            placeholder={placeholder}
+            clearInput={clearInput}
+            required={required}
+            disabled={disabled}
+            readonly={readonly}
+            className={error ? 'ion-invalid' : ''}
+          />
+        );
+    }
+  };
+
   return (
     <>
-      <IonItem className={className}>
-        <IonInput
-          label={label}
-          labelPlacement="floating"
-          type={type}
-          value={value}
-          onIonChange={(e) => onChange(e.detail.value || '')}
-          placeholder={placeholder}
-          clearInput={clearInput}
-          required={required}
-          disabled={disabled}
-          readonly={readonly}
-          className={error ? 'ion-invalid' : ''}
-        />
-      </IonItem>
+      <IonItem className={className}>{renderInput()}</IonItem>
       {error && (
         <IonText color="danger" className="ion-padding-start">
           <small>{error}</small>
@@ -54,3 +122,5 @@ export const FormField: React.FC<IProps> = ({
     </>
   );
 };
+
+export { FormField };
