e5b52b90f622cd188c6beb05da52fe6aa7407e3d
guiazca <guiazca@gmail.com>
2024-11-18 20:10:00 +0000

    remove comments

--- playwright/login.spec.js
+++ playwright/login.spec.js
@@ -1,7 +1,6 @@
-// tests/example.spec.js
 import { test, expect } from '@playwright/test';
 
 test('Teste simples de exemplo', async ({ page }) => {
   await page.goto('/');
-  await expect(page).toHaveTitle(/Ionic App/); // Verifica se o título contém "Seu Título"
+  await expect(page).toHaveTitle(/Ionic App/);
 });
