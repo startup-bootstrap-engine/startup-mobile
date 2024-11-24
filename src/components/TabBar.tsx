810e4f245b60cebce355c289b5f64fd36467c0eb
Fernando Rocha <fernandorochaworld@gmail.com>
2024-11-24 11:33:24 -0800

    Fixed the nested router issue.

--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -8,7 +8,6 @@ import {
 } from '@ionic/react';
 import React from 'react';
 import { PrivateRoute } from './PrivateRoute';
-import { IonReactRouter } from '@ionic/react-router';
 
 export interface ITabConfig {
   route: string;
@@ -23,28 +22,26 @@ interface IProps {
 
 export const TabBar: React.FC<IProps> = ({ tabs }) => {
   return (
-    <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet id="tabs-router">
-          {tabs.map(({ route, component: Component }) => (
-            <PrivateRoute
-              key={`route-${route}`}
-              path={route}
-              component={Component}
-              exact={true}
-            />
-          ))}
-        </IonRouterOutlet>
+    <IonTabs>
+      <IonRouterOutlet id="tabs-router">
+        {tabs.map(({ route, component: Component }) => (
+          <PrivateRoute
+            key={`route-${route}`}
+            path={route}
+            component={Component}
+            exact={true}
+          />
+        ))}
+      </IonRouterOutlet>
 
-        <IonTabBar slot="bottom">
-          {tabs.map(({ route, label, icon }) => (
-            <IonTabButton key={route} tab={route} href={route}>
-              <IonIcon icon={icon} />
-              <IonLabel>{label}</IonLabel>
-            </IonTabButton>
-          ))}
-        </IonTabBar>
-      </IonTabs>
-    </IonReactRouter>
+      <IonTabBar slot="bottom">
+        {tabs.map(({ route, label, icon }) => (
+          <IonTabButton key={route} tab={route} href={route}>
+            <IonIcon icon={icon} />
+            <IonLabel>{label}</IonLabel>
+          </IonTabButton>
+        ))}
+      </IonTabBar>
+    </IonTabs>
   );
 };
